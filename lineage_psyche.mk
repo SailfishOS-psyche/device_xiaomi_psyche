399c033a4236a15622e57031c5a410b72843980c
johnmart19 <johnivan19999@gmail.com>
2024-02-18 21:13:23 +0200

    psyche: Initial Xiaomi 12X bringup
    Missing: UDFPS, proprietary-files.txt

--- lineage_psyche.mk
+++ lineage_psyche.mk
@@ -18,15 +18,15 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 # Build Type Flags
 EVO_BUILD_TYPE = DEVELOPMENT
 
-# Inherit from alioth device
-$(call inherit-product, device/xiaomi/alioth/device.mk)
+# Inherit from psyche device
+$(call inherit-product, device/xiaomi/psyche/device.mk)
 
-PRODUCT_NAME := lineage_alioth
-PRODUCT_DEVICE := alioth
+PRODUCT_NAME := lineage_psyche
+PRODUCT_DEVICE := psyche
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_BRAND := POCO
-PRODUCT_MODEL := POCO F3
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Xiaomi 12X
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-BUILD_FINGERPRINT := POCO/alioth_global/alioth:13/TKQ1.221114.001/V816.0.2.0.TKHMIXM:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/psyche/psyche:13/TKQ1.221114.001/V816.0.3.0.TLDCNXM:user/release-keys
